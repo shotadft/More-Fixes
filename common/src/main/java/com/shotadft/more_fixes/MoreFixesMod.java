7329459375ad7e81f3d930c82858c732c4ab3fb7
Shotadft <98450322+shotadft@users.noreply.github.com>
2026-07-12 23:05:45 +0900

    Initial Commit

---
+++ common/src/main/java/com/shotadft/more_fixes/MoreFixesMod.java
@@ -0,0 +1,9 @@
+package com.shotadft.more_fixes;
+
+public final class MoreFixesMod {
+    public static final String MOD_ID = "more_fixes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/shotadft/more_fixes/neoforge/MoreFixesModNeoForge.java
@@ -0,0 +1,13 @@
+package com.shotadft.more_fixes.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.shotadft.more_fixes.MoreFixesMod;
+
+@Mod(MoreFixesMod.MOD_ID)
+public final class MoreFixesModNeoForge {
+    public MoreFixesModNeoForge() {
+        // Run our common setup.
+        MoreFixesMod.init();
+    }
+}
